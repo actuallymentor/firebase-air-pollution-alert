747a78e711ed0a7b44bef04199c72c94f62526ea
Mentor <mentor@palokaj.co>
2017-03-27 20:57:27 +0200

    Readme update

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
 // Grab styles
 import styles from '../css/styles.scss'
+
+// Import firebase
+import firebase from 'firebase'
+
+firebase.initializeApp( {
+	apiKey: process.env.apiKey,
+	authDomain: process.env.authDomain,
+	databaseURL: process.env.databaseURL,
+	storageBucket: process.env.storageBucket,
+	messagingSenderId: process.env.messagingSenderId
+} )
